bbc59175ed537838e43f904af750489caa4fc5aa
Whisky92 <olah.tomi.92@gmail.com>
2022-12-03 23:51:05 +0100

    Adding unit tests

--- ticket-service/src/test/java/com/epam/training/ticketservice/core/services/serviceimpl/RoomServiceImplTest.java
+++ ticket-service/src/test/java/com/epam/training/ticketservice/core/services/serviceimpl/RoomServiceImplTest.java
@@ -1,11 +1,18 @@
 package com.epam.training.ticketservice.core.services.serviceimpl;
 
 import com.epam.training.ticketservice.core.entity.RoomEntity;
+import com.epam.training.ticketservice.core.model.MovieDTO;
+import com.epam.training.ticketservice.core.model.RoomDTO;
 import com.epam.training.ticketservice.core.repository.RoomRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class RoomServiceImplTest {
@@ -17,5 +24,19 @@ public class RoomServiceImplTest {
     private RoomServiceImpl roomService;
 
     private final RoomEntity TEST_ROOM = new RoomEntity("Room1",12,12);
+    @Test
+    void testCreateMovieShouldReturnOptionalEmptyWhenMovieTitleDoesNotExist() {
+        //Given
+        Optional<MovieDTO> expected = Optional.empty();
+        Mockito.when(roomRepository.findByName(TEST_ROOM.getName()))
+                .thenReturn(Optional.empty());
+        //When
+        Optional<RoomDTO> actual = roomService
+                .createRoom(TEST_ROOM.getName(), TEST_ROOM.getRowCount(), TEST_ROOM.getColumnCount());
+        //Then
+        Assertions.assertEquals(expected,actual);
+        Mockito.verify(roomRepository).findByName(TEST_ROOM.getName());
+    }
+
 
 }
